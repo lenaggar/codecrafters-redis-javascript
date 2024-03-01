19931b105bca1f33a7d9be9ae2c887be745095ed
Mahmoud ElNaggar <mahmoud.elnaggar@datasnipper.com>
2024-03-01 21:35:30 +0100

    Handle concurrent clients

--- app/main.js
+++ app/main.js
@@ -1,9 +1,12 @@
 const net = require("net");
 
+const PORT = 6379;
+const HOST_NAME = "127.0.0.1";
+
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     connection.write("+PONG\r\n");
   });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(PORT, HOST_NAME);
